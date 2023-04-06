20b2ab309b0bee32db2dc49945f371f62e95368d
Thomas Tresansky <ttresansky@gradle.com>
2023-04-05 21:41:35 -0400

    Try simplified check

---
+++ lib/src/main/kotlin/com/tomtresansky/gitstream/Library10.kt
@@ -0,0 +1,5 @@
+package com.tomtresansky.gitstream
+
+class Library10 {
+    fun add(a: Int, b: Int): Int = a + b
+}
---
+++ lib/src/main/kotlin/com/tomtresansky/gitstream/Library9.kt
@@ -0,0 +1,5 @@
+package com.tomtresansky.gitstream
+
+class Library9 {
+    fun add(a: Int, b: Int): Int = a + b
+}
