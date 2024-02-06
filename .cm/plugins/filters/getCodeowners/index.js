d612619b5d21d4259c33f1f2d68f171ea6d96810
Tom Tresansky <ttresansky@gradle.com>
2024-02-06 10:48:49 -0500

    Restore full functionality

--- .cm/plugins/filters/getCodeowners/index.js
+++ .cm/plugins/filters/getCodeowners/index.js
@@ -4,97 +4,68 @@
  * @param {string[]} files - the gitStream's files context variable
  * @param {Object} pr - the gitStream's pr context variable
  * @param {string} token - access token with repo:read scope, used to read the CODEOWNERS file
+ * @param {string} pathToCodeOwners - path from repo root to CODEOWNERS file
  * @returns {string[]} user names
- * @example {{ files | getCodeowners(pr, env.CODEOWNERS_TOKEN) }}
- * @license MIT
+ * @example {{ files | getCodeowners(pr, env.CODEOWNERS_TOKEN, '.github/CODEOWNERS') }}
  **/
 
-
 const { Octokit } = require("@octokit/rest");
-// const ignore = require('./ignore/index.js');
+const ignore = require('./ignore/index.js');
 
-async function loadCodeownersFile(owner, repo, auth) {
+async function loadCodeownersFile(owner, repo, auth, pathToCodeOwners) {
     const octokit = new Octokit({
         request: { fetch },
         auth,
     });
 
-    console.log("ahead of getContent, token: " + auth)
     const res = await octokit.repos.getContent({
         owner,
         repo,
-        path: '.github/CODEOWNERS'
+        path: pathToCodeOwners
     });
-    console.log("Done getContent")
-    console.log("Content: " + res)
 
     return Buffer.from(res.data.content, 'base64').toString()
 }
-//
-// function codeownersMapping(data) {
-//     return data
-//         .toString()
-//         .split('\n')
-//         .filter(x => x && !x.startsWith('#'))
-//         .map(x => x.split("#")[0])
-//         .map(x => {
-//             const line = x.trim();
-//             const [path, ...owners] = line.split(/\s+/);
-//             return {path, owners};
-//         });
-// }
-//
-// function resolveCodeowner(mapping, file) {
-//     const match = mapping
-//         .slice()
-//         .reverse()
-//         .find(x =>
-//             ignore()
-//                 .add(x.path)
-//                 .ignores(file)
-//         );
-//     if (!match) return false;
-//     return match.owners;
-// }
+function codeownersMapping(data) {
+    return data
+        .toString()
+        .split('\n')
+        .filter(x => x && !x.startsWith('#'))
+        .map(x => x.split("#")[0])
+        .map(x => {
+            const line = x.trim();
+            const [path, ...owners] = line.split(/\s+/);
+            return {path, owners};
+        });
+}
 
-// module.exports = {
-//     async: true,
-//     filter: async (files, pr, token, callback) => {
-//         console.log("TESTING ASYNCHRONOUS FILTERS")
-//
-//         // const fileData = await loadCodeownersFile(pr.author, pr.repo, token);
-//         // console.log("Done fileData")
-//         // console.log("fileData", fileData)
-//         // const mapping = codeownersMapping(fileData);
-//         // console.log("Done codeownersMapping")
-//         // console.log("codeownersMapping", mapping)
-//         //
-//         // const resolved = files
-//         //     .map(f => resolveCodeowner(mapping, f))
-//         //     .flat()
-//         //     .filter(i => typeof i === 'string')
-//         //     .map(u => u.replace(/^@/, ""));
-//         //
-//         // const unique = [...new Set(resolved)];
-//
-//         return callback(null, ["heyo"]);
-//     },
-// }
-
-const myFilter = async (files, pr, token, callback) => {
-    const message = { text: "Hello ${files}!" };
-    const error = null;
-    console.log(message)
-    console.log(token)
-
-    const fileData = await loadCodeownersFile(pr.author, pr.repo, token);
-    console.log("Done fileData")
-    console.log("fileData", fileData)
-
-    return callback(error, message.text);
-};
+function resolveCodeowner(mapping, file) {
+    const match = mapping
+        .slice()
+        .reverse()
+        .find(x =>
+            ignore()
+                .add(x.path)
+                .ignores(file)
+        );
+    if (!match) return false;
+    return match.owners;
+}
 
 module.exports = {
     async: true,
-    filter: myFilter
+    filter: async (files, pr, token, pathToCodeOwners, callback) => {
+        const fileData = await loadCodeownersFile(pr.author, pr.repo, token, pathToCodeOwners);
+        const mapping = codeownersMapping(fileData);
+
+        const resolved = files
+            .map(f => resolveCodeowner(mapping, f))
+            .flat()
+            .filter(i => typeof i === 'string')
+            .map(u => u.replace(/^@/, ""));
+
+        const unique = [...new Set(resolved)];
+
+        return callback(null, unique);
+    },
 }
