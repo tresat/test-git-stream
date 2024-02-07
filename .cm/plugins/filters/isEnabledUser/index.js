2a2ffe1da68309f96917c277190147c5a33c7a3a
Tom Tresansky <ttresansky@gradle.com>
2024-02-07 09:14:07 -0500

    Add summarize

--- .cm/plugins/filters/isEnabledUser/index.js
+++ .cm/plugins/filters/isEnabledUser/index.js
@@ -8,7 +8,7 @@ const jvm = ["big-guy", "ghale", "jvandort", "octylFractal", "tresat"];
 /**
  * @module isEnabledUser
  * @description Returns true if the username that is passed to this function is a member of the Gradle BT Team who has opted into gitStream automations.
- * @param {string} Input - The GitHub username to check.
+ * @param {string} username - The GitHub username to check.
  * @returns {boolean} Returns true if the user is specified in the flaggedUsers list, otherwise false.
  * @example {{ pr.author | isEnabledUser }}
  */
---
+++ .cm/plugins/filters/summarizeFileStatistics/index.js
@@ -0,0 +1,15 @@
+/**
+ * @module summarizeFileStatistics
+ * @description Summarizes .
+ * @param {Map} fileGroups -
+ * @returns {boolean} Returns .
+ * @example {{ filesByPlatform | summarizeFileStatistics }}
+ */
+function summarizeFileStatistics(fileGroups) {
+    let totalAdditions = 0;
+    let totalDeletions = 0;
+
+    return fileGroups;
+};
+
+module.exports = summarizeFileStatistics;
