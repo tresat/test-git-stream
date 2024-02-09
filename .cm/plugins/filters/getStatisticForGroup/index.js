38b9301e687c588f4a187630c3eef1c76aa63194
Tom Tresansky <ttresansky@gradle.com>
2024-02-09 08:17:29 -0500

    Add getStatByGroup

---
+++ .cm/plugins/filters/getStatisticForGroup/index.js
@@ -0,0 +1,16 @@
+/**
+ * @module getStatisticForGroup
+ * @description Returns
+ * @param {[Object]} statistics -
+ * @param {String} groupName -
+ * @param {String} statisticName -
+ * @returns {[Object]} Returns a list of objects for each platform containing info about the changes to files in that platform
+ * @example {{ statsByPlatform | getByName('jvm', 'name') }}
+ */
+
+function getStatisticForGroup(fileMetadatas, groupName, statisticName) {
+
+    return 'red';
+}
+
+module.exports = getStatisticForGroup;
