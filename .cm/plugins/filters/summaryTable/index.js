19146f41bbfb83fdc513796d3c4029e3f3f4fedd
Tom Tresansky <ttresansky@gradle.com>
2024-04-02 11:02:27 -0400

    restore

--- .cm/plugins/filters/summaryTable/index.js
+++ .cm/plugins/filters/summaryTable/index.js
@@ -21,36 +21,35 @@ function summaryTable(statistics) {
     console.log("New Ratio: " + newRatio);
 
     let result = `:bar_chart: **Change Summary: this PR is ${Math.round(newRatio, 2)}% new code**
-    
-         ${platformsAffected(preppedStatistics)}`;
-    //     <details>
-    //     <summary>See details</summary>
-    //     <table>
-    //         <tr>
-    //             <td>Platform</td>
-    //             <td>Added Lines</td>
-    //             <td>% of Total Line Changes</td>
-    //             <td>Deleted Lines</td>
-    //             <td>% of Total Line Changes</td>
-    //             <td>Files Changed</td>
-    //             <td>% of Total Files Changed</td>
-    //         </tr>`;
-    //
-    // preppedStatistics.forEach(summary => {
-    //     result += `<tr>
-    //         <td>${summary.name}</td>
-    //         <td>${summary.additions}</td>
-    //         <td>${summary.additionPercent}%</td>
-    //         <td>${summary.deletions}</td>
-    //         <td>${summary.deletionPercent}%</td>
-    //         <td>${summary.files.length}</td>
-    //         <td>${summary.filesPercent}%</td>
-    //     </tr>`;
-    // });
-    //
-    // result += `</table>
-    //     </details>
-    //     <automation id="summary_table/summary_table"/>`;
+         ${platformsAffected(preppedStatistics)}
+        <details>
+        <summary>See details</summary>
+        <table>
+            <tr>
+                <td>Platform</td>
+                <td>Added Lines</td>
+                <td>% of Total Line Changes</td>
+                <td>Deleted Lines</td>
+                <td>% of Total Line Changes</td>
+                <td>Files Changed</td>
+                <td>% of Total Files Changed</td>
+            </tr>`;
+
+    preppedStatistics.forEach(summary => {
+        result += `<tr>
+            <td>${summary.name}</td>
+            <td>${summary.additions}</td>
+            <td>${summary.additionPercent}%</td>
+            <td>${summary.deletions}</td>
+            <td>${summary.deletionPercent}%</td>
+            <td>${summary.files.length}</td>
+            <td>${summary.filesPercent}%</td>
+        </tr>`;
+    });
+
+    result += `</table>
+        </details>
+        <automation id="summary_table/summary_table"/>`;
     console.log("Finished summaryTable: " + result);
 
     return result;
