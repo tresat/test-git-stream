bcc19729e796743e1ea37663c767209eab196639
Tom Tresansky <ttresansky@gradle.com>
2024-04-02 09:49:27 -0400

    aga

--- .cm/plugins/filters/summaryTable/index.js
+++ .cm/plugins/filters/summaryTable/index.js
@@ -17,35 +17,35 @@ function summaryTable(statistics) {
     let totalDeletions = preppedStatistics.reduce((acc, summary) => acc + summary.deletions, 0);
     let newRatio = totalAdditions / (totalAdditions + totalDeletions) * 100;
 
-    let result = `:bar_chart: **Change Summary: this PR is ${Math.round(newRatio, 2)}% new code**
+    // let result = `:bar_chart: **Change Summary: this PR is ${Math.round(newRatio, 2)}% new code**
+    //
+    //     ${platformsAffected(preppedStatistics)}
+    //     <details>
+    //     <summary>See details</summary>
+    //     <table>
+    //         <tr>
+    //             <td>Platform</td>
+    //             <td>Added Lines</td>
+    //             <td>% of Total Line Changes</td>
+    //             <td>Deleted Lines</td>
+    //             <td>% of Total Line Changes</td>
+    //             <td>Files Changed</td>
+    //             <td>% of Total Files Changed</td>
+    //         </tr>`;
+    //
+    // preppedStatistics.forEach(summary => {
+    //     result += `<tr>
+    //         <td>${summary.name}</td>
+    //         <td>${summary.additions}</td>
+    //         <td>${summary.additionPercent}%</td>
+    //         <td>${summary.deletions}</td>
+    //         <td>${summary.deletionPercent}%</td>
+    //         <td>${summary.files.length}</td>
+    //         <td>${summary.filesPercent}%</td>
+    //     </tr>`;
+    // });
 
-        ${platformsAffected(preppedStatistics)}
-        <details>
-        <summary>See details</summary>
-        <table>
-            <tr>
-                <td>Platform</td>
-                <td>Added Lines</td>
-                <td>% of Total Line Changes</td>
-                <td>Deleted Lines</td>
-                <td>% of Total Line Changes</td>
-                <td>Files Changed</td>
-                <td>% of Total Files Changed</td>
-            </tr>`;
-
-    preppedStatistics.forEach(summary => {
-        result += `<tr>
-            <td>${summary.name}</td>
-            <td>${summary.additions}</td>
-            <td>${summary.additionPercent}%</td>
-            <td>${summary.deletions}</td>
-            <td>${summary.deletionPercent}%</td>
-            <td>${summary.files.length}</td>
-            <td>${summary.filesPercent}%</td>
-        </tr>`;
-    });
-
-    result += `</table>
+    let result = `</table>
         </details>
         <automation id="summary_table/summary_table"/>`;
 
@@ -53,28 +53,27 @@ function summaryTable(statistics) {
 }
 
 function platformsAffected(statistics) {
-    // // Significance is defined as a platform having more than 10% of the total lines changed
-    // let totalLinesChanged = statistics.reduce((acc, summary) => acc + summary.additions + summary.deletions, 0);
-    // let platformHasSignificantChanges = function(summary) {
-    //     let linesChanged = summary.additions + summary.deletions;
-    //     return (linesChanged > 1) && (linesChanged / totalLinesChanged >= 0.1);
-    // }
-    // let platformsWithSignificantChanges = statistics.filter(platformHasSignificantChanges);
-    //
-    // let result = "";
-    // if (statistics.length > 1) {
-    //     result += statistics.length + " platforms were affected";
-    // } else {
-    //     result += "1 platform was affected";
-    // }
-    // if (platformsWithSignificantChanges.length > 1) {
-    //     result += " :warning: (If possible, only one platform should have significant changes in a PR)";
-    // } else {
-    //     result += " :white_check_mark:";
-    // }
-    //
-    // return result;
-    return "PLATFORMS AFFECTED";
+    // Significance is defined as a platform having more than 10% of the total lines changed
+    let totalLinesChanged = statistics.reduce((acc, summary) => acc + summary.additions + summary.deletions, 0);
+    let platformHasSignificantChanges = function(summary) {
+        let linesChanged = summary.additions + summary.deletions;
+        return (linesChanged > 1) && (linesChanged / totalLinesChanged >= 0.1);
+    };
+    let platformsWithSignificantChanges = statistics.filter(platformHasSignificantChanges);
+
+    let result = "";
+    if (statistics.length > 1) {
+        result += statistics.length + " platforms were affected";
+    } else {
+        result += "1 platform was affected";
+    }
+    if (platformsWithSignificantChanges.length > 1) {
+        result += " :warning: (If possible, only one platform should have significant changes in a PR)";
+    } else {
+        result += " :white_check_mark:";
+    }
+
+    return result;
 }
 
 module.exports = summaryTable;
