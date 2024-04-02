333e7738e34917678a96bc3b4c9f9780051a529b
Tom Tresansky <ttresansky@gradle.com>
2024-04-02 16:53:56 -0400

    Test

--- .cm/plugins/filters/groupByPlatform/index.js
+++ .cm/plugins/filters/groupByPlatform/index.js
@@ -1,13 +1,13 @@
 /**
  * @module groupByPlatform
  * @description Returns a mapping of platforms to the information about the files involved in the PR contained in each platform
- * @param {[FileMetadata]} fileMetadatas - gitStream's list of metadata about file changes in the PR including path
  * @param {Map} fileOwners mapping of platform name to a list of files involved in the change located in that platform
+ * @param {[FileMetadata]} fileMetadatas - gitStream's list of metadata about file changes in the PR including path
  * @returns {[Object]} Returns a list of objects for each platform containing info about the changes to files in that platform
  * @example {{ branch.diff.files_metadata | getPlatforms }}
  */
 
-function groupByPlatform(fileMetadatas, fileOwners) {
+function groupByPlatform(fileOwners, fileMetadatas) {
     const filesByPlatform = new Map();
     [...fileOwners.keys()].forEach(platform => {
         filesByPlatform.set(platform, {
